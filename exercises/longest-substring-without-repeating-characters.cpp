a9e212eb5764e6e650a272fd323f10dd8c5f815e
adrian <adriansemmler@gmx.de>
2024-11-19 23:40:50 +0100

    two exercises

---
+++ exercises/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,85 @@
+/*
+
+Given a string s, find the length of the longest
+substring without repeating characters.
+
+
+Example 1:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+Input: s = "pwwkew"
+Output: 3
+
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+Constraints:
+
+0 <= s.length <= 5 * 10^4
+s consists of English letters, digits, symbols and spaces.
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int lengthOfLongestSubstring(string str)
+{
+    vector<char> longestSubStringFound;
+    vector<char> subString;
+
+    for (int i = 0; i < str.length() - 1; i++)
+    {
+        subString = {};
+        subString.push_back(str[i]);
+
+        for (int j = i + 1; j < str.length(); j++)
+        {
+            vector<char>::iterator it = find(subString.begin(), subString.end(), str[j]);
+            if (it == subString.end()) // char was not found in subString
+            {
+                subString.push_back(str[j]);
+            }
+            else if (longestSubStringFound.size() < subString.size())
+            {
+                longestSubStringFound = subString;
+                subString = {};
+                break;
+            }
+        }
+    }
+
+    string answerString(longestSubStringFound.begin(), longestSubStringFound.end());
+    cout << answerString << endl;
+
+    return longestSubStringFound.size();
+}
+
+int main()
+{
+    string inputOne = "abcabcbb";
+    string inputTwo = "bbbbb";
+    string inputThree = "pwwkew";
+
+    int answerOne = lengthOfLongestSubstring(inputOne);
+    int answerTwo = lengthOfLongestSubstring(inputTwo);
+    int answerThree = lengthOfLongestSubstring(inputThree);
+
+    cout << answerOne << endl;
+    cout << answerTwo << endl;
+    cout << answerThree << endl;
+
+    return 0;
+}
---
+++ exercises/two-sum.cpp
@@ -0,0 +1,82 @@
+/*
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+2 <= nums.length <= 10^4
+-10^9 <= nums[i] <= 10^9
+-10^9 <= target <= 10^9
+
+Only one valid answer exists.
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    vector<int> retVal;
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums.at(i) + nums.at(j) == target)
+            {
+                retVal = {i, j};
+                break;
+            }
+        }
+    }
+    return retVal;
+}
+
+// chatgpt improved solution O(n)
+vector<int> twoSumGpt(vector<int> &nums, int target)
+{
+    unordered_map<int, int> numMap; // Maps a number to its index
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int complement = target - nums[i];
+        // Check if the complement exists in the map
+        if (numMap.find(complement) != numMap.end())
+        {
+            return {numMap[complement], i};
+        }
+        // Add the current number and its index to the map
+        numMap[nums[i]] = i;
+    }
+    return {}; // Shouldn't reach here as the problem guarantees a solution
+}
+
+int main()
+{
+    vector<int> nums = {12, 17, 2, 7, 11, 15};
+    vector<int> answer = twoSum(nums, 9);
+
+    std::cout << "Solution: {" + to_string(answer.at(0)) + ", " + to_string(answer.at(1)) + "}" << endl;
+
+    return 0;
+}
