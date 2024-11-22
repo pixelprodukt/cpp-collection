e786327d4c9296f198cfb9952df6229955af9db6
Adrian Semmler <adrian.semmler@wdw-elab.de>
2024-11-22 22:04:39 +0100

    some palindromic code

--- include/longest-palindromic-substring.hpp
+++ include/longest-palindromic-substring.hpp
@@ -2,7 +2,9 @@
 #define LONGEST_PALINDROMIC_SUBSTRING_HPP
 
 #include <iostream>
+#include <vector>
 
+bool startAndEndAreSameChar(const std::vector<char> &vec);
 std::string longestPalindrome(std::string str);
 
 #endif
--- src/longest-palindromic-substring.cpp
+++ src/longest-palindromic-substring.cpp
@@ -1,8 +1,8 @@
 /*
 
-Given a string s, return the longest 
+Given a string s, return the longest
 palindromic
- 
+
 substring
  in s.
 
@@ -15,18 +15,57 @@ Explanation: "aba" is also a valid answer.
 Example 2:
 Input: s = "cbbd"
 Output: "bb"
- 
+
 
 Constraints:
 1 <= s.length <= 1000
 s consist of only digits and English letters.
 
 */
+#include "../include/longest-palindromic-substring.hpp"
+
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+bool startAndEndAreSameChar(const vector<char> &vec) {
+    return (vec.at(0) == vec.at(vec.size() - 1));
+}
+
 string longestPalindrome(string str) {
+    vector<char> buffer = {};
+
+    for (char c : str) {
+        buffer.push_back(c);
+
+        /* if (buffer.size() <= 1) {
+            continue;
+        } */
+
+        for (int i = buffer.size(); i < str.length() - 1; i++) {
+
+            if (startAndEndAreSameChar(buffer)) {
+                cout << "is same: " << buffer.at(0) << " " << str[i] << endl;
+
+                vector<char> shortenedBuf = buffer;
+                shortenedBuf.pop_back();
+                shortenedBuf.erase(shortenedBuf.begin());
+
+                cout << "address of buffer: " << &buffer << endl;
+                cout << "address of shortenedList: " << &shortenedBuf << endl;
+
+                // check
+            }
+
+            // at the end, reset buffer
+            buffer.clear();
+        }
+    }
+
+    // vs code setting: C_Cpp.clang_format_fallbackStyle -> copy as json -> { BasedOnStyle: Google, IndentWidth: 4, ColumnLimit: 0}
+
     string retval = "hello";
     return retval;
 }
